ee8cc39232ae2f5444914027c81435ff6e1b3f63
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-06-02 23:48:51 +0600

    .

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+    return (
+        <div className="flex flex-col gap-4 w-full min-h-screen p-10">
+            <div className="skeleton h-32 w-full"></div>
+            <div className="skeleton h-4 w-28"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full mb-5"></div>
+            <div className="skeleton h-4 w-1/2"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+        </div>
+    )
+}
