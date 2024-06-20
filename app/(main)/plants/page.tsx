fc60675eb5095914a1cd70d2d5402cdb81467c70
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-06-20 10:15:29 +0600

    tf

--- app/(main)/plants/page.tsx
+++ app/(main)/plants/page.tsx
@@ -1,6 +1,5 @@
 import Loading from '@/app/loading';
 import { Pagination, PlantCard } from '@/components/main';
-import { FlaskConicalOff, PawPrint } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,7 +7,7 @@ interface PlantCategory {
     id: number;
     name: string;
     description: string;
-    image: string;
+    image: string | null;
 }
 
 interface Tag {
@@ -24,13 +23,12 @@ interface Plant {
     size: string;
     description: string;
     care_instructions: string;
-    features: { id: number, name: string }[];
-    tags: Tag[];
-    created_at: string;
+    features?: { id: number; name: string }[];
+    tags?: Tag[];
     images: { id: number; image: string }[];
 }
 
-export default async function Plants(context: any) {
+export default async function Pages(context: any) {
     const page = context.searchParams.page ? context.searchParams.page : 1;
     const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/plants/?page=${page}`, {
         cache: "no-cache",
@@ -47,19 +45,15 @@ export default async function Plants(context: any) {
     const baseURL = 'plants/';
 
     if (!plants.length) {
-        return <div>
-            <Loading />
-        </div>;
+        return <Loading />;
     }
 
     return (
         <>
-            <h1 className="text-3xl font-bold mb-6">
-                Browse all Plants
-            </h1>
+            <h1 className="text-3xl font-bold mb-6">Browse all Plants</h1>
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4 lg:p-2 mb-16">
                 {plants.map((plant) => (
-                    <PlantCard plant={plant} key={plant.id} />
+                    <PlantCard key={plant.id} plant={plant} />
                 ))}
             </div>
             <div className="fixed bottom-4 right-4">
--- components/main/cards/PlantCard.tsx
+++ components/main/cards/PlantCard.tsx
@@ -5,7 +5,7 @@ interface PlantCategory {
     id: number;
     name: string;
     description: string;
-    image: string;
+    image: string | null;
 }
 
 interface Tag {
@@ -21,30 +21,27 @@ interface Plant {
     size: string;
     description: string;
     care_instructions: string;
-    features: { id: number, name: string }[];
-    tags: Tag[];
-    created_at: string;
+    features?: { id: number; name: string }[];
+    tags?: Tag[];
     images: { id: number; image: string }[];
 }
 
-type PlantCardProps = {
-    plant: Plant;
-};
-
-export default function PlantCard({ plant }: PlantCardProps) {
+export default function PlantCard(
+    { plant }: { plant: Plant }
+) {
     return (
         <div className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 lg:hover:scale-105">
             <Link href={`/plants/${plant.id}`}>
                 <div className="relative">
                     <Image
-                        src={plant.images && plant.images.length > 0 ? plant.images[0].image : "/static/no-img.png"}
+                        src={plant.images.length > 0 ? plant.images[0].image : "/static/no-img.png"}
                         alt={plant.title}
                         height={227}
                         width={384}
                         className="h-56 w-full object-cover"
                     />
                     <div className="absolute top-2 left-2 py-1 px-2 rounded-sm backdrop-blur-md bg-gray-800 bg-opacity-50 text-white">
-                        {plant.features.map(feature => (
+                        {plant.features?.map(feature => (
                             <span key={feature.id} className="text-xs font-medium mr-1">
                                 {feature.name}
                             </span>
