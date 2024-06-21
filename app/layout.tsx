cc3f18951ff87c1b631058bc25e8ca2880725ecd
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-06-22 01:44:14 +0600

    <SpeedInsights /> added

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className}`}>
         {children}
         <ChatFab />
+        <SpeedInsights />
       </body>
     </html>
   );
