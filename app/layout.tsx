bc62f098c026e87005ec6fbdf3f28dddd9ad54a1
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-06-21 15:29:10 +0600

    add vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ChatFab } from "@/components/common";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
