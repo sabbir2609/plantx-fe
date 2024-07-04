0d34440144f96b918e1b1e615382e6532aff5f08
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-07-04 12:53:03 +0600

    pwa added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,55 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ["latin"] });
+const APP_NAME = "Viriditas";
+const APP_DEFAULT_TITLE = "Elevate your space with Nature";
+const APP_TITLE_TEMPLATE = "%s - Viriditas";
+const APP_DESCRIPTION = "Elevate your space with Nature";
 
 export const metadata: Metadata = {
-  title: "Viriditas",
-  description: "Viriditas - Your one-stop shop for all things plants and planters",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,33 @@
-/** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
 
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'sabbir2609.pythonanywhere.com',
-            },
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-            },
-            {
-                protocol: 'http',
-                hostname: 'openweathermap.org',
-            },
-            {
-                protocol: 'https',
-                hostname: 'sorsauth.sirv.com',
-            },
-
-        ],
-    },
-};
-export default nextConfig;
+export default withPWA({
+  // Your Next.js config
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sabbir2609.pythonanywhere.com",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "openweathermap.org",
+      },
+      {
+        protocol: "https",
+        hostname: "sorsauth.sirv.com",
+      },
+    ],
+  },
+});
