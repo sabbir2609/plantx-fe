ba806efab4dc0e4bbc6c4dbce55b66b1d6827c9e
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-07-11 00:24:30 +0600

    .

--- components/common/ChatFab.tsx
+++ components/common/ChatFab.tsx
@@ -2,7 +2,6 @@
 
 import { MessageSquareDot } from 'lucide-react';
 import React from 'react';
-import Image from 'next/image';
 
 export default function ChatPlugin() {
     const handleChatFabClick = () => {
@@ -11,14 +10,9 @@ export default function ChatPlugin() {
 
     return (
         <div className='fixed bottom-4 right-0 px-4 z-40'>
-            <button onClick={handleChatFabClick} className='bg-base-100 hover:bg-base-200 font-bold p-2 rounded-full shadow-lg'>
-                <Image
-                    height={24}
-                    width={24}
-                    src="/static/messenger_logo.svg"
-                    alt="Chat Icon"
-                    className='h-10 w-10' />
-                {/* <MessageSquareDot size={24} className="inline-block" /> */}
+            <button onClick={handleChatFabClick} className='bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded-full shadow-lg'>
+                {/* <img src="/static/messenger_logo.svg" alt="Chat Icon" className='h-10 w-10' /> */}
+                <MessageSquareDot size={24} className="inline-block" />
             </button>
         </div >
     );
