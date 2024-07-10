587ccb2ca0bd83f27aae8bbc98457b3ee6c892ce
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-07-11 00:21:24 +0600

    refactor: Update ChatFab component to use Next.js Image component for better performance

--- app/(main)/plants/[id]/page.tsx
+++ app/(main)/plants/[id]/page.tsx
@@ -36,68 +36,67 @@ export default async function Plants({ params }: { params: { id: number } }) {
     const plant: Plant = data;
 
     return (
-        <div className='mx-auto p-2'>
-            <div className="flex flex-wrap mx-auto">
-                <div className="w-full lg:w-1/2">
-                    {plant.images.length > 0 ? (
-                        <SwiperSlideComponent images={plant.images} />
-                    ) : (
-                        <div className="">
-                            <Image
-                                src="/static/no-img.png"
-                                height={800}
-                                width={800}
-                                className="object-cover rounded-lg"
-                                alt="Placeholder"
-                            />
-                        </div>
-                    )}
-                    <div className="p-2 rounded-lg shadow-md">
-                        <div className="flex flex-row gap-2 py-2">
-                            {plant.features.map(feature => (
-                                <p key={feature.id} className="inline-block p-1 text-sm text-green-700 bg-green-100 rounded-sm">
-                                    {feature.name}
-                                </p>
-                            ))}
-                            <p className="inline-block p-1 text-sm text-green-700 rounded-sm bg-amber-200">
-                                {plant.indoor_or_outdoor}
+        <div className="flex flex-wrap mx-auto p-2 gap-4 lg:gap-0">
+            <div className="w-full lg:w-1/2">
+                {plant.images.length > 0 ? (
+                    <SwiperSlideComponent images={plant.images} />
+                ) : (
+                    <div className="">
+                        <Image
+                            src="/static/no-img.png"
+                            height={800}
+                            width={800}
+                            className="object-cover rounded-lg"
+                            alt="Placeholder"
+                        />
+                    </div>
+                )}
+                <div className="p-2 rounded-lg shadow-md">
+                    <div className="flex flex-row gap-2 py-2">
+                        {plant.features.map(feature => (
+                            <p key={feature.id} className="inline-block p-1 text-sm text-green-700 bg-green-100 rounded-sm">
+                                {feature.name}
                             </p>
-                            <p className="inline-block p-1 text-sm rounded-sm bg-lime-200 text-lime-700">
-                                {plant.size}
-                            </p>
-                        </div>
-                        <h1 className="text-2xl font-bold">{plant.title}</h1>
-                        <p className="text-xl">{plant.category.name}</p>
-                        <div className="">
-                            <p className="text-sm"><span className="text-lg font-semibold"></span>{plant.tags.map(tag => tag.name).join(', ')}</p>
-                        </div>
+                        ))}
+                        <p className="inline-block p-1 text-sm text-green-700 rounded-sm bg-amber-200">
+                            {plant.indoor_or_outdoor}
+                        </p>
+                        <p className="inline-block p-1 text-sm rounded-sm bg-lime-200 text-lime-700">
+                            {plant.size}
+                        </p>
+                    </div>
+                    <h1 className="text-2xl font-bold">{plant.title}</h1>
+                    <p className="text-xl">{plant.category.name}</p>
+                    <div className="">
+                        <p className="text-sm"><span className="text-lg font-semibold"></span>{plant.tags.map(tag => tag.name).join(', ')}</p>
                     </div>
                 </div>
-                <div className="lg:ps-2 w-full lg:w-1/2 space-y-2">
-                    <div className="join join-vertical w-full">
-                        <div className="collapse collapse-arrow join-item border-base-300 bg-base-200 border">
-                            <input type="radio" name="my-accordion-4" defaultChecked />
-                            <div className="collapse-title text-xl font-medium">
-                                Description
-                            </div>
-                            <div className="collapse-content overflow-x-hidden bg-base-100">
-                                <div
-                                    className="prose max-h-[57vh]"
-                                    dangerouslySetInnerHTML={{ __html: plant.description }}
-                                />
-                            </div>
+            </div>
+
+            <div className="lg:ps-2 w-full lg:w-1/2 space-y-2">
+                <div className="join join-vertical w-full">
+                    <div className="collapse collapse-arrow join-item border-base-300 bg-base-200 border">
+                        <input type="radio" name="my-accordion-4" defaultChecked />
+                        <div className="collapse-title text-xl font-medium">
+                            Description
                         </div>
-                        <div className="collapse collapse-arrow join-item border-base-300 bg-base-200 border">
-                            <input type="radio" name="my-accordion-4" />
-                            <div className="collapse-title text-xl font-medium">
-                                Care Instructions
-                            </div>
-                            <div className="collapse-content overflow-x-hidden bg-base-100">
-                                <div
-                                    className="prose max-h-[57vh]"
-                                    dangerouslySetInnerHTML={{ __html: plant.care_instructions }}
-                                />
-                            </div>
+                        <div className="collapse-content overflow-x-hidden bg-base-100">
+                            <div
+                                className="prose max-h-[57vh]"
+                                dangerouslySetInnerHTML={{ __html: plant.description }}
+                            />
+                        </div>
+                    </div>
+                    <div className="collapse collapse-arrow join-item border-base-300 bg-base-200 border">
+                        <input type="radio" name="my-accordion-4" />
+                        <div className="collapse-title text-xl font-medium">
+                            Care Instructions
+                        </div>
+                        <div className="collapse-content overflow-x-hidden bg-base-100">
+                            <div
+                                className="prose max-h-[57vh]"
+                                dangerouslySetInnerHTML={{ __html: plant.care_instructions }}
+                            />
                         </div>
                     </div>
                 </div>
--- components/common/ChatFab.tsx
+++ components/common/ChatFab.tsx
@@ -2,18 +2,23 @@
 
 import { MessageSquareDot } from 'lucide-react';
 import React from 'react';
+import Image from 'next/image';
 
 export default function ChatPlugin() {
     const handleChatFabClick = () => {
-        // Open the chat link
         window.open('https://m.me/350812004777614/');
     };
 
     return (
         <div className='fixed bottom-4 right-0 px-4 z-40'>
-            <button onClick={handleChatFabClick} className='bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded-full shadow-lg'>
-                {/* <img src="/static/messenger_logo.svg" alt="Chat Icon" className='h-10 w-10' /> */}
-                <MessageSquareDot size={24} className="inline-block" />
+            <button onClick={handleChatFabClick} className='bg-base-100 hover:bg-base-200 font-bold p-2 rounded-full shadow-lg'>
+                <Image
+                    height={24}
+                    width={24}
+                    src="/static/messenger_logo.svg"
+                    alt="Chat Icon"
+                    className='h-10 w-10' />
+                {/* <MessageSquareDot size={24} className="inline-block" /> */}
             </button>
         </div >
     );
