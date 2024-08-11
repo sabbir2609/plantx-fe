cc59c811e9bbd0ed60ef0110716a4ac29b2d06ce
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-08-12 01:12:10 +0600

    refactor: Update contact page content and styling

--- app/(homepage)/contact/page.tsx
+++ app/(homepage)/contact/page.tsx
@@ -47,9 +47,9 @@ export default function Page() {
             <div className="hero-content p-0 flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Contact Us!</h1>
-                    <p className="py-6">We would love to hear from you. Please fill out the form below and we will get back to you as soon as possible.</p>
+                    <p className="py-4 px-2">We would love to hear from you. Please fill out the form below and we will get back to you as soon as possible.</p>
                 </div>
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-200">
                     <form className="card-body" onSubmit={handleSubmit}>
                         <div className="form-control">
                             <label className="label">
