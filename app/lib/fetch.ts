01217b51f11a2364c345da70fe937d6b746341af
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2024-09-22 17:13:15 +0600

    Refactor fetch function to handle different environments

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -1,25 +1,38 @@
 "use server";
 
 export default async function Fetch({ endpoint }: { endpoint: string }) {
-    // console.log(`Fetching data from ${process.env.NEXT_PUBLIC_HOST}/${endpoint}`);
+    const url = `${process.env.NEXT_PUBLIC_HOST}/${endpoint}`;
 
-
-    // const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/${endpoint}`, {
-    //     next: {
-    //         revalidate: 3600,
-    //     },
-    // });
-
-
-    const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/${endpoint}`,
-        {
+    // if dev
+    if (process.env.NODE_ENV === "development") {
+        console.log(`Fetching data from ${url} in development mode`);
+        const response = await fetch(url, {
             cache: "no-cache",
-        }
-    );
+        });
 
-    if (!response.ok) {
-        throw new Error("Failed to fetch data");
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        return data;
     }
-    const data = await response.json();
-    return data;
+
+    // if prod
+    else if (process.env.NODE_ENV === "production") {
+        const response = await fetch(url, {
+            next: {
+                revalidate: 3600,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        return data;
+    } else {
+        throw new Error("Unknown environment");
+    }
 }
--- app/manifest.ts
+++ app/manifest.ts
@@ -9,7 +9,36 @@ export default function manifest(): MetadataRoute.Manifest {
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#000000',
+        scope: '/',
+        orientation: 'portrait',
+        lang: 'en-US',
+        dir: 'ltr',
         icons: [
+            {
+                src: '/icons/icon-72x72.png',
+                sizes: '72x72',
+                type: 'image/png',
+            },
+            {
+                src: '/icons/icon-96x96.png',
+                sizes: '96x96',
+                type: 'image/png',
+            },
+            {
+                src: '/icons/icon-128x128.png',
+                sizes: '128x128',
+                type: 'image/png',
+            },
+            {
+                src: '/icons/icon-144x144.png',
+                sizes: '144x144',
+                type: 'image/png',
+            },
+            {
+                src: '/icons/icon-152x152.png',
+                sizes: '152x152',
+                type: 'image/png',
+            },
             {
                 src: '/icons/icon-192x192.png',
                 sizes: '192x192',
@@ -18,6 +47,23 @@ export default function manifest(): MetadataRoute.Manifest {
             {
                 src: '/icons/icon-512x512.png',
                 sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+        screenshots: [
+            {
+                src: '/screenshots/screenshot_1.png',
+                sizes: '640x480',
+                type: 'image/png',
+            },
+            {
+                src: '/screenshots/screenshot_2.png',
+                sizes: '640x480',
+                type: 'image/png',
+            },
+            {
+                src: '/screenshots/screenshot_3.png',
+                sizes: '640x480',
                 type: 'image/png',
             },
         ],
