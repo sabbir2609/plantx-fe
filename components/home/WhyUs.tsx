1b0372d519f9305cd0c732fc275e356ba16df1af
Sabbir Hasan Munna <theviriditas@gmail.com>
2024-09-27 02:20:43 +0600

    Refactor WhyUs component and add animated sections

--- components/home/WhyUs.tsx
+++ components/home/WhyUs.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { motion } from "framer-motion";
 import {
     Lightbulb,
     Settings,
@@ -9,6 +6,7 @@ import {
     CheckCircle,
     Heart,
 } from "lucide-react";
+import AnimatedSection from "./Wrapper/AnimatedSection";
 
 export default function WhyChooseUs() {
     const whyUs = [
@@ -29,7 +27,7 @@ export default function WhyChooseUs() {
         },
         {
             title: "Eco-Friendly Practices",
-            description: "We Priorities Using Sustainable Materials & Methods.",
+            description: "We Prioritize Using Sustainable Materials & Methods.",
             icon: Leaf,
         },
         {
@@ -45,38 +43,26 @@ export default function WhyChooseUs() {
     ];
 
     return (
-        <div className="relative py-16 overflow-hidden">
+        <div className="relative py-14 overflow-hidden">
             {/* Background decoration */}
-            <div className="absolute inset-0 bg-[url('/images/bg/leaf-pattern.webp')] opacity-20 pointer-events-none"></div>
+            <div className="absolute inset-0 bg-[url('/images/bg/leaf-pattern.webp')] bg-cover opacity-50 blur-sm"></div>
             {/* Content container */}
-            <div className="relative z-10 max-w-7xl mx-auto px-6 sm:px-8 lg:px-12">
-                <motion.h2
-                    initial={{ opacity: 0, y: 50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8 }}
-                    className="text-center text-4xl font-bold mb-10"
-                >
-                    Why Choose Us
-                </motion.h2>
-
+            <div className="mx-auto p-4">
+                <h2 className="text-center text-4xl font-bold mb-10">Why Choose Us</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {whyUs.map((item, index) => (
-                        <motion.div
-                            key={index}
-                            initial={{ opacity: 0, y: 20 }}
-                            whileInView={{ opacity: 1, y: 0 }}
-                            viewport={{ once: true }}
-                            transition={{ duration: 0.5, delay: index * 0.2 }}
-                            className="p-6 rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-xl backdrop-filter backdrop-blur-lg"
-                        >
-                            <div className="flex items-center mb-4">
-                                <item.icon className="w-8 h-8 text-green-700 dark:text-green-300" />
-                                <h3 className="ml-4 text-2xl font-semibold">{item.title}</h3>
+                        <AnimatedSection key={index} delay={index * 0.2}>
+                            <div
+                                className="flex flex-col p-6 rounded-lg shadow-lg h-full bg-base-300">
+                                <div className="flex items-center mb-4">
+                                    <item.icon className="w-8 h-8 text-green-700 dark:text-green-300" />
+                                    <h3 className="ml-4 text-xl lg:text-2xl font-semibold">{item.title}</h3>
+                                </div>
+                                <p className="font-normal text-lg leading-relaxed flex-grow">
+                                    {item.description}
+                                </p>
                             </div>
-                            <p className="font-normal text-lg leading-relaxed">
-                                {item.description}
-                            </p>
-                        </motion.div>
+                        </AnimatedSection>
                     ))}
                 </div>
             </div>
