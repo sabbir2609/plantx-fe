f9cc0404d881ec6541a17e48c76bac1d4c78b464
Sabbir Hasan Munna <theviriditas@gmail.com>
2024-11-20 01:00:23 +0600

    Refactor About and Testimonial components for improved background color and padding

--- components/home/Testimonial.tsx
+++ components/home/Testimonial.tsx
@@ -5,8 +5,8 @@ export default async function Testimonial() {
     const data = await Fetch({ endpoint: 'main/testimonials/' });
     if (data.length === 0) return null;
     return (
-        <section className="p-3 mx-auto mt-2 mb-4">
-            <header className="mt-4 text-center">
+        <section className="p-3 mx-auto mt-2 mb-4 pb-14">
+            <header className="mt-4 text-center pb-8">
                 <h2 className={`text-3xl font-bold`}>
                     Customer
                 </h2>
