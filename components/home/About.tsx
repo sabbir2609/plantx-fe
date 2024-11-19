2888bb83345a6dc2ed6d51dda119ed081d566eef
Sabbir Hasan Munna <theviriditas@gmail.com>
2024-11-20 00:36:54 +0600

    Refactor About component for improved background styling

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -8,7 +8,7 @@ const Heading_Font = Luckiest_Guy({
 
 export default function About() {
     return (
-        <p className="p-2 py-12 text-lg lg:text-2xl text-center bg-base-200 leading-6">
+        <p className="p-2 py-12 text-lg lg:text-2xl text-center bg-base-300 leading-6">
             At <span className='text-xl font-bold'>Viriditas,</span> we specialize in green design for interiors and landscapes, bringing nature into homes, workplaces, and public spaces. With eco-friendly practices and innovative approaches, we create beautiful, sustainable spaces that enrich communities and the environment.
         </p>
     );
