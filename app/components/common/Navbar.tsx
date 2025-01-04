90b7dde3368f2ddaa01a6f9b3d526d7101d67e6a
theviriditas <theviriditas@gmail.com>
2025-01-05 02:46:05 +0600

    Enhance Navbar component with additional links and social media icons for improved navigation

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -1,7 +1,24 @@
 "use client";
 
-import React from "react";
-import { ChevronDownIcon, Facebook, Instagram, Twitter } from "lucide-react";
+import React, { JSX } from "react";
+import {
+  Home,
+  ChevronRight,
+  Cylinder,
+  HandPlatter,
+  HelpCircle,
+  Leaf,
+  MessageSquareHeart,
+  MessageSquareText,
+  Rss,
+  Sprout,
+  UsersRound,
+  Youtube,
+  Facebook,
+  Instagram,
+  Twitter,
+  ChevronDownIcon,
+} from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -9,47 +26,89 @@ interface LinkItem {
   id: number;
   name: string;
   link: string;
+  icon?: JSX.Element;
   defaultOpen?: boolean;
+  isDeviderAtEnd?: boolean;
+  hideOnlargeScreen?: boolean;
   sublinks?: { name: string; link: string }[];
 }
 
+interface SocialLink {
+  name: string;
+  link: string;
+  icon: JSX.Element;
+}
+
 // Navigation links
-const links: LinkItem[] = [
-  {
-    id: 1,
-    name: "Home",
-    link: "/",
-  },
+const navLinks: LinkItem[] = [
   {
     id: 2,
-    name: "About",
-    link: "/about",
+    name: "Services",
+    link: "/services",
+    icon: <HandPlatter size={20} className="inline-block" />,
     defaultOpen: true,
     sublinks: [
-      { name: "Our Story", link: "/about/story" },
-      { name: "Team", link: "/about/team" },
+      { name: "All Services", link: "/services" },
+      { name: "Commercial", link: "/services/commercial" },
+      { name: "Residential", link: "/services/residential" },
     ],
   },
   {
     id: 3,
-    name: "Services",
-    link: "/services",
+    name: "Blog",
+    link: "/blog",
+    icon: <Leaf size={20} className="inline-block" />,
+    defaultOpen: false,
     sublinks: [
-      { name: "Service 1", link: "/services/service-1" },
-      { name: "Service 2", link: "/services/service-2" },
+      { name: "All Posts", link: "/blog" },
+      { name: "Categories", link: "/blog/category" },
     ],
+    isDeviderAtEnd: true,
   },
   {
     id: 4,
-    name: "Contact",
+    name: "About",
+    link: "/about",
+    icon: <UsersRound size={20} className="inline-block" />,
+  },
+  {
+    id: 5,
+    name: "Contact Us",
     link: "/contact",
+    icon: <MessageSquareHeart size={20} className="inline-block" />,
+  },
+  {
+    id: 6,
+    name: "Help",
+    link: "/help",
+    hideOnlargeScreen: true,
+    icon: <HelpCircle size={20} className="inline-block" />,
+  },
+];
+
+// Social links
+const socialLinks: SocialLink[] = [
+  {
+    name: "Youtube",
+    link: "https://www.youtube.com/@ViriditasInterior",
+    icon: <Youtube />,
+  },
+  {
+    name: "Facebook",
+    link: "https://facebook.com/theviriditas",
+    icon: <Facebook />,
+  },
+  {
+    name: "Instagram",
+    link: "https://instagram.com/viriditas",
+    icon: <Instagram />,
   },
 ];
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
   const [activeIndex, setActiveIndex] = useState<number | null>(() => {
-    const index = links.findIndex((link) => link.defaultOpen);
+    const index = navLinks.findIndex((link) => link.defaultOpen);
     return index === -1 ? null : index;
   });
 
@@ -75,29 +134,36 @@ export default function Navbar() {
         </Link>
 
         <div className="hidden space-x-10 text-white md:flex">
-          {links.map((link) => (
-            <div key={link.id} className="group relative">
-              <Link href={link.link} className="flex items-center space-x-4">
-                <span className="link-underline link-underline-black font-medium">
-                  {link.name}
-                </span>
-              </Link>
+          <div className="hidden space-x-10 text-white md:flex">
+            {navLinks
+              .filter((link) => !link.hideOnlargeScreen)
+              .map((link) => (
+                <div key={link.id} className="group relative">
+                  <Link
+                    href={link.link}
+                    className="flex items-center space-x-4"
+                  >
+                    <span className="link-underline link-underline-black font-medium">
+                      {link.name}
+                    </span>
+                  </Link>
 
-              {link.sublinks && (
-                <div className="absolute left-0 top-full hidden rounded-sm bg-base-300 px-6 group-hover:block">
-                  {link.sublinks.map((sublink, index) => (
-                    <Link
-                      key={index}
-                      href={sublink.link}
-                      className="sublink-hover block text-nowrap p-2 hover:text-primary"
-                    >
-                      {sublink.name}
-                    </Link>
-                  ))}
+                  {link.sublinks && (
+                    <div className="absolute left-0 top-full hidden rounded-sm bg-base-300 px-6 group-hover:block">
+                      {link.sublinks.map((sublink, index) => (
+                        <Link
+                          key={index}
+                          href={sublink.link}
+                          className="sublink-hover block text-nowrap p-2 hover:text-primary"
+                        >
+                          {sublink.name}
+                        </Link>
+                      ))}
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          ))}
+              ))}
+          </div>
         </div>
 
         <div className={`z-40 md:hidden ${open ? "z-40 text-white" : ""}`}>
@@ -132,13 +198,20 @@ export default function Navbar() {
 
           {/* colapsibe manu  */}
           <div className="h-full overflow-y-auto p-6">
-            {links.map((link, index) => (
+            {navLinks.map((link, index) => (
               <div key={link.id} className="mb-4">
                 <div
                   className="flex cursor-pointer items-center justify-between"
                   onClick={() => toggleMenu(index)}
                 >
-                  <span className="font-semibold">{link.name}</span>
+                  <span className="flex items-center text-nowrap font-semibold">
+                    {link.icon && (
+                      <span className="mr-2 flex items-center">
+                        {link.icon}
+                      </span>
+                    )}
+                    {link.name}
+                  </span>
                   {link.sublinks && (
                     <span
                       className={`transition-transform duration-500 ease-in-out ${
@@ -149,6 +222,7 @@ export default function Navbar() {
                     </span>
                   )}
                 </div>
+
                 {link.sublinks && (
                   <div
                     className={`ml-4 mt-2 overflow-hidden transition-all duration-500 ease-in-out ${
@@ -168,35 +242,28 @@ export default function Navbar() {
                     ))}
                   </div>
                 )}
+
+                {link.isDeviderAtEnd && (
+                  <div className="divider divider-primary"></div>
+                )}
               </div>
             ))}
           </div>
 
-          <div className="flex justify-around border-t border-base-200 bg-base-300 p-3">
-            <Link
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className=""
-            >
-              <Facebook />
-            </Link>
-            <Link
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className=""
-            >
-              <Twitter />
-            </Link>
-            <Link
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className=""
-            >
-              <Instagram />
-            </Link>
+
+                      <div className="flex justify-around border-t border-base-200 bg-base-300 p-3">
+            {socialLinks.map((link, index) => (
+              <Link
+                key={index}
+                href={link.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-primary"
+              >
+                {link.icon}
+              </Link>
+            ))}
+
           </div>
         </div>
       </div>
