1f30d52615f42a0f425701325472051efce4029b
theviriditas <theviriditas@gmail.com>
2025-01-06 00:50:38 +0600

    Refactor Commercial and Residential service pages for improved structure and styling

--- app/(main)/services/commercial/page.tsx
+++ app/(main)/services/commercial/page.tsx
@@ -1,56 +1,56 @@
-import { Fetch } from '@/app/lib';
-import Image from 'next/image';
-import Link from 'next/link';
-import type { Metadata } from 'next'
+import { Fetch } from "@/app/lib";
+import Image from "next/image";
+import Link from "next/link";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Commercial Services',
-    description: 'Commercial services offered by Viriditas',
-}
+  title: "Commercial Services",
+  description: "Commercial services offered by Viriditas",
+};
 
 interface Category {
-    id: number;
-    title: string;
-    slug: string;
-    type: string;
-    image: string;
+  id: number;
+  title: string;
+  slug: string;
+  type: string;
+  image: string;
 }
 
 async function getCommercialCategories() {
-    const data = await Fetch({ endpoint: 'main/service_categories/commercial/' });
-    return data;
+  const data = await Fetch({ endpoint: "main/service_categories/commercial/" });
+  return data;
 }
 
 export default async function Page() {
+  const commercialCategories: Category[] = await getCommercialCategories();
 
-    const commercialCategories: Category[] = await getCommercialCategories();
-
-    return (
-        <div className="mx-auto p-2">
-
-            <h2 className="mb-2 text-2xl font-semibold">
-                Commercial Service Categories
-            </h2>
-            <div className="grid grid-cols-2 gap-4 mb-4 md:grid-cols-2 lg:grid-cols-4">
-                {commercialCategories.map((category) => (
-                    <Link
-                        key={category.id}
-                        href={`/services/commercial/${category.slug}`}
-                        className="group relative block h-48 overflow-hidden rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
-                    >
-                        <Image
-                            src={category.image || '/static/viriditas.webp'}
-                            alt={category.title}
-                            height={200}
-                            width={200}
-                            className="transition duration-300 ease-in-out group-hover:opacity-75 h-full w-full object-cover"
-                        />
-                        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40">
-                            <h2 className="text-xl font-bold text-white text-center">{category.title}</h2>
-                        </div>
-                    </Link>
-                ))}
+  return (
+    <div className="mx-auto max-w-screen-xl p-4">
+      <h2 className="mb-4 text-2xl font-semibold">
+        Commercial Service Categories
+      </h2>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {commercialCategories.map((category) => (
+          <Link
+            key={category.id}
+            href={`/services/commercial/${category.slug}`}
+            className="group relative block h-48 overflow-hidden rounded-lg bg-base-200 shadow-lg transition-transform duration-300 ease-in-out hover:scale-105"
+          >
+            <Image
+              src={category.image || "/static/viriditas.webp"}
+              alt={category.title}
+              height={200}
+              width={200}
+              className="h-full w-full object-cover transition-opacity duration-300 ease-in-out group-hover:opacity-75"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 transition-colors duration-300 group-hover:bg-opacity-50">
+              <h2 className="m-2 text-center text-xl font-bold text-white">
+                {category.title}
+              </h2>
             </div>
-        </div>
-    );
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/(main)/services/residential/[slug]/page.tsx
+++ app/(main)/services/residential/[slug]/page.tsx
@@ -1,75 +1,78 @@
 import { Fetch } from "@/app/lib";
-import Loading from "@/app/loading";
 import { ServiceCard } from "@/app/components/main";
 import Image from "next/image";
 
 interface MainCategory {
-    id: number;
-    title: string;
-    slug: string;
-    type: string;
-    description: string;
-    image: string;
+  id: number;
+  title: string;
+  slug: string;
+  type: string;
+  description: string;
+  image: string;
 }
 
 interface Category {
-    id: number;
-    type: string;
-    title: string;
+  id: number;
+  type: string;
+  title: string;
 }
 
 interface Service {
-    id: number;
-    title: string;
-    slug: string;
-    image: string;
-    categories: Category[];
-    tags: string[];
+  id: number;
+  title: string;
+  slug: string;
+  image: string;
+  categories: Category[];
+  tags: string[];
 }
 
 async function fetchCategory(slug: string) {
-    const data = await Fetch({ endpoint: `main/service_categories/${slug}` });
-    return data;
+  const data = await Fetch({ endpoint: `main/service_categories/${slug}` });
+  return data;
 }
 
 async function fetchPlants(slug: string) {
-    const data = await Fetch({ endpoint: `main/service_categories/${slug}/services` });
-    return data.results;
+  const data = await Fetch({ endpoint: `main/service_categories/${slug}/services` });
+  return data.results;
 }
 
 export default async function Page(props: { params: Promise<{ slug: string }> }) {
-    const params = await props.params;
-    const category: MainCategory = await fetchCategory(params.slug);
-    const services: Service[] = await fetchPlants(params.slug);
+  const params = await props.params;
+  const category: MainCategory = await fetchCategory(params.slug);
+  const services: Service[] = await fetchPlants(params.slug);
 
-    return (
-        <div className="mx-auto p-2">
-            <div className="relative h-auto mb-4 overflow-hidden rounded-lg shadow-lg bg-base-200 lg:h-80">
-                <Image
-                    src={category.image || '/static/viriditas.webp'}
-                    height={800}
-                    width={800}
-                    alt={category.title}
-                    className="object-cover w-full h-full"
-                />
-                <div className="absolute bottom-0 left-0 content-center h-full p-4 text-white bg-black bg-opacity-50 md:w-1/2">
-                    <h1 className="text-3xl font-semibold">{category.title}</h1>
-                    <h2 className="text-xl">{category.type}</h2>
-                    <p className="mt-2">{category.description}</p>
-                </div>
-            </div>
-            <h2 className="text-2xl font-semibold">Services</h2>
-            {services.length === 0 ? (
-                <div className="p-4 text-center">
-                    Oops! No service found in this category yet :(
-                </div>
-            ) : (
-                <div className="grid grid-cols-2 gap-2 p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-5">
-                    {services.map((service) => (
-                        <ServiceCard service={service} key={service.id} />
-                    ))}
-                </div>
-            )}
+  return (
+    <div className="mx-auto max-w-screen-xl p-4">
+      {/* Banner */}
+      <div className="relative mb-6 overflow-hidden rounded-lg bg-base-200 shadow-lg lg:h-80">
+        <Image
+          src={category.image || "/static/viriditas.webp"}
+          height={800}
+          width={800}
+          alt={category.title}
+          priority
+          className="h-full w-full object-cover transition-transform duration-300 ease-in-out hover:scale-105"
+        />
+        <div className="absolute bottom-0 left-0 flex h-full w-full flex-col justify-end bg-black/50 p-4 text-white md:w-1/2">
+          <h1 className="mb-1 text-3xl font-bold">{category.title}</h1>
+          <h2 className="text-lg capitalize">{category.type}</h2>
+          <p className="mt-2 text-sm">{category.description}</p>
         </div>
-    );
+      </div>
+
+      {/* Services */}
+      <h2 className="mb-4 text-2xl font-semibold">Services</h2>
+      {services.length === 0 ? (
+        <div className="rounded-lg bg-base-200 p-6 text-center text-gray-500 shadow-md">
+          Oops! No services found in this category
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-5">
+          {services.map((service) => (
+            <ServiceCard service={service} key={service.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
